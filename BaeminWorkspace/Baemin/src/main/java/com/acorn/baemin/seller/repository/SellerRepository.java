b40f8c533b1d885d4064aff10d096c6af36eabb7
taemin817 <tmpark11@naver.com>
2023-11-07 15:21:29 +0900

    form + 해야함

--- BaeminWorkspace/Baemin/src/main/java/com/acorn/baemin/seller/repository/SellerRepository.java
+++ BaeminWorkspace/Baemin/src/main/java/com/acorn/baemin/seller/repository/SellerRepository.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.acorn.baemin.domain.MenuDTO;
 import com.acorn.baemin.domain.ReviewDTO;
 import com.acorn.baemin.domain.SellerDTO;
 import com.acorn.baemin.domain.StoreDTO;
@@ -19,38 +20,41 @@ public class SellerRepository implements SellerRepositoryI {
 	private static String namespaceReview = "com.acorn.ReviewMapper.";
 	private static String namespaceAnswer = "com.acorn.AnswerMapper.";
 
-	// 준태
-	
-	
-	   
 
 	// 태민
-	// 매장정보탭
+	// 메뉴 탭
+	// 메뉴 전체 조회
+	@Override
+	public List<MenuDTO> selectAllMenuInfo() {
+		System.out.println("selectMenuInfo @repo");
+		return session.selectList(namespaceSeller + "selectMenuInfo");
+	}
+	// 매장정보 탭
 	// 매장정보 일부조회1(매장이름, 매장평점, 리뷰수, 최소주문금액)
 	@Override
 	public StoreDTO selectStoreSubInfo1(int storeCode) {
-		System.out.println(storeCode);
-		System.out.println("repo : selectStoreSubInfo1");
+		System.out.println("storeCode @repo : " + storeCode);
+		System.out.println("selectStoreSubInfo1 @repo");
 		return session.selectOne(namespaceSeller + "selectStoreSubInfo1", storeCode);
 	}
 	// 매장정보 일부조회2(가게소개, 운영시간, 매장주소)
 	@Override
 	public StoreDTO selectStoreSubInfo2(int storeCode) {
-		System.out.println(storeCode);
-		System.out.println("repo : selectStoreSubInfo2");
+		System.out.println("storeCode @repo : " + storeCode);
+		System.out.println("selectStoreSubInfo2 @repo");
 		return session.selectOne(namespaceSeller + "selectStoreSubInfo2", storeCode);
 	}
 	// 사장님정보 일부조회(이름, 사업자등록번호)
 	@Override
 	public SellerDTO selectSellerSubInfo(int sellerCode) {
-		System.out.println(sellerCode);
-		System.out.println("repo : selectSellerSubInfo");
+		System.out.println("sellerCode @repo : " + sellerCode);
+		System.out.println("selectSellerSubInfo @repo");
 		return session.selectOne(namespaceSeller + "selectSellerSubInfo", sellerCode);
 	}
 	// 매장정보 일부수정(가게소개, 운영시간, 매장주소)
 	@Override
 	public int updateStoreSubInfo(StoreDTO store) {
-		System.out.println("repo : updateStoreSubInfo");
+		System.out.println("updateStoreSubInfo @repo");
 		return session.selectOne(namespaceSeller + "updateStoreSubInfo", store);
 	}
 	// 사장님정보 일부수정(이름, 사업자등록번호)
@@ -59,28 +63,29 @@ public class SellerRepository implements SellerRepositoryI {
 		System.out.println("repo : updateSellerSubInfo");
 		return session.update(namespaceSeller + "updateSellerSubInfo", seller);
 	}
+	// 리뷰 탭
 	// 리뷰 전체 조회
 	@Override
 	public List<ReviewDTO> selectAllReview() {
-		System.out.println("repo : selectAllReview");
+		System.out.println("selectAllReview @repo");
 		return session.selectList(namespaceReview + "selectAllReview");
 	}
 	// 답변 등록
 	@Override
 	public int insertAnswer(String AnswerContent) {
-		System.out.println("repo : insertAnswer");
+		System.out.println("insertAnswer @repo");
 		return session.insert(namespaceAnswer + "insertAnswer", AnswerContent);
 	}
 	// 답변 수정
 	@Override
 	public int updateAnswer(String AnswerContent) {
-		System.out.println("repo : updateAnswer");
+		System.out.println("updateAnswer @repo");
 		return session.update(namespaceAnswer + "updateAnswer", AnswerContent);
 	}
 	// 답변 삭제
 	@Override
 	public int deleteAnswer(int AnswerCode) {
-		System.out.println("deleteAnswer");
+		System.out.println("deleteAnswer @repo");
 		return session.delete(namespaceAnswer + "deleteAnswer", AnswerCode);
 	}
 
--- BaeminWorkspace/Baemin/src/main/java/com/acorn/baemin/seller/service/SellerService.java
+++ BaeminWorkspace/Baemin/src/main/java/com/acorn/baemin/seller/service/SellerService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.acorn.baemin.domain.MenuDTO;
 import com.acorn.baemin.domain.ReviewDTO;
 import com.acorn.baemin.domain.SellerDTO;
 import com.acorn.baemin.domain.StoreDTO;
@@ -17,63 +18,71 @@ public class SellerService implements ServiceI{
 	SellerRepository rep;
 	
 	// 태민
-	// 매장정보탭
+	// 메뉴 탭
+	// 메뉴 전체 조회
+	@Override
+	public List<MenuDTO> selectAllMenuInfo() {
+		System.out.println("selectMenuInfo @service");
+		List<MenuDTO> list =  rep.selectAllMenuInfo();
+		return list;
+	}
+	// 매장정보 탭
 	// 매장정보 일부조회1(매장이름, 평점, 리뷰수, 최소주문금액)
 	@Override
 	public StoreDTO selectStoreNmRtRcMp(int storeCode) {
-		System.out.println("service : selectStoreNmRtRcMp");
-		System.out.println(storeCode);
+		System.out.println("storeCode @service : " + storeCode);
+		System.out.println("selectStoreNmRtRcMp @service");
 		return rep.selectStoreSubInfo1(storeCode);
 	}
 	// 매장정보 일부조회2(가게소개, 운영시간, 매장주소)
 	@Override
 	public StoreDTO selectStoreDTA(int storeCode) {
-		System.out.println("service : selectStoreDTA");
-		System.out.println(storeCode);
+		System.out.println("storeCode @service : " + storeCode);
+		System.out.println("selectStoreDTA @service");
 		return rep.selectStoreSubInfo2(storeCode);
 	}
 	// 사장님정보 일부조회(이름, 사업자등록번호)
 	@Override
 	public SellerDTO selectSellerNmRn(int sellerCode) {
-		System.out.println("service : selectSellerNmRn");
-		System.out.println(sellerCode);
+		System.out.println("storeCode @service : " + sellerCode);
+		System.out.println("selectSellerNmRn @service");
 		return rep.selectSellerSubInfo(sellerCode);
 	}
 	// 매장정보 일부수정(가게소개, 운영시간, 매장주소)
 	@Override
 	public void modifingStoreDTA(StoreDTO store) {
-		System.out.println("service : modifingStoreDTA");
+		System.out.println("modifingStoreDTA @service");
 		rep.updateStoreSubInfo(store);
 	}
 	// 사장님정보 일부수정(이름, 사업자등록번호)
 	@Override
 	public void modifingSellerNmRn(SellerDTO seller) {
-		System.out.println("service : modifingSellerNmRn");
+		System.out.println("modifingSellerNmRn @service");
 		rep.updateSellerSubInfo(seller);
 	}
-	// 리뷰탭
+	// 리뷰 탭
 	// 리뷰 전체 조회
 	@Override
 	public List<ReviewDTO> selectAllReview() {
-		System.out.println("service : selectAllReview");
+		System.out.println("selectAllReview @service");
 		return rep.selectAllReview();
 	}
 	// 답변 등록
 	@Override
 	public void insertAnswer(String AnswerContent) {
-		System.out.println("service : selectAllReview");
+		System.out.println("selectAllReview @service");
 		rep.insertAnswer(AnswerContent);
 	}
 	// 답변 수정
 	@Override
 	public void updateAnswer(String AnswerContent) {
-		System.out.println("service : selectAllReview");
+		System.out.println("selectAllReview @service");
 		rep.updateAnswer(AnswerContent);
 	}
 	// 답변 삭제
 	@Override
 	public void deleteAnswer(int AnswerCode) {
-		System.out.println("service : selectAllReview");
+		System.out.println("selectAllReview @service");
 		rep.deleteAnswer(AnswerCode);
 	}
 
