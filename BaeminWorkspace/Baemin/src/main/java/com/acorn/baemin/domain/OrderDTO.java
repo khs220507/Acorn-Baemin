05a9f2a1b0470ef49219608c289f17b15d3dcdef
taemin817 <tmpark11@naver.com>
2023-11-16 17:08:44 +0900

    정보탭 조회, 수정 기능 완료  리뷰 탭 남음

--- BaeminWorkspace/Baemin/src/main/java/com/acorn/baemin/domain/OrderDTO.java
+++ BaeminWorkspace/Baemin/src/main/java/com/acorn/baemin/domain/OrderDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDTO {
-	int  orderNumber;
+	int orderNumber;
 	int userCode;
 	int storeCode;
 	String orderMenuName;
