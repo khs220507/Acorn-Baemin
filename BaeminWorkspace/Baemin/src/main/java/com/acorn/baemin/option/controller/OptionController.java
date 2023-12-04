bfb7cb9186979011a3b57d44473f284e37b6edb1
juntae710 <pjt010914@gmail.com>
2023-12-04 15:31:45 +0900

    이미지경로 수정

--- BaeminWorkspace/Baemin/src/main/java/com/acorn/baemin/option/controller/OptionController.java
+++ BaeminWorkspace/Baemin/src/main/java/com/acorn/baemin/option/controller/OptionController.java
@@ -27,7 +27,7 @@ public class OptionController {
 	@Autowired
 	OptionRepository rep;
 	
-	//user
+	//유저 옵션조회
 	@GetMapping("/option")
 	public String selectOption(@RequestParam String menuCode, Model model) {
 
@@ -47,7 +47,7 @@ public class OptionController {
 		return "store/option";
 		}
 	
-	//seller
+	//사장님 옵션조회
 	@GetMapping("/sellerOption")
 	public String sellerOption(@RequestParam String menuCode, Model model) {
 		MenuDTO MenuInfo= rep.selectMenuInfo(menuCode);
@@ -60,36 +60,37 @@ public class OptionController {
 		return "seller/menu_option";
 		}
 	
+	//옵션 추가
 	@ResponseBody
 	@RequestMapping( value="/sellerOptionSolo" , method=RequestMethod.POST)
 	public void insertOptions(@RequestBody OptionDTO Option) {
 		rep.insertOption(Option);
 	}
-	
+	//옵션 카테고리 추가
 	@ResponseBody
 	@RequestMapping( value="/sellerOption" , method=RequestMethod.POST)
 	public void insertOption(@RequestBody OptionDTO Option) {
 		rep.insertOption(Option);
 	}
-	
+	//옵션 수정
 	@ResponseBody
 	@RequestMapping( value="/sellerOptionSolo" , method=RequestMethod.PUT)
 	public void updateOption(@RequestBody OptionDTO Option) {
 		rep.updateOption(Option);
 	}
-	
+	//옵션 카테고리 수정
 	@ResponseBody
 	@RequestMapping( value="/sellerOption" , method=RequestMethod.PUT)
 	public void updateOptionCategory(@RequestBody OptionDTO Option) {
 		rep.updateOptionCategory(Option);
 	}
-	
+	//옵션 삭제
 	@ResponseBody
 	@RequestMapping( value="/sellerOptionDSolo/{optionCode}" , method=RequestMethod.PUT)
 	public void deleteOption(@PathVariable String optionCode) {
 		rep.deleteOption(optionCode);
 	}
-	
+	//옵션 카테고리 삭제
 	@ResponseBody
 	@RequestMapping( value="/sellerOptionD/{menuCode}/{category}" , method=RequestMethod.PUT)
 	public void deleteOptionCategory(@PathVariable int menuCode, @PathVariable String category) {
